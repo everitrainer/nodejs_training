3856df909964073a58b3df956515188d17977e85
“lakshmikant” <fullstacktrainer4u@gmail.com>
2024-08-08 14:47:20 +0530

    Changes in express

--- web_server/express/app.js
+++ web_server/express/app.js
@@ -22,14 +22,27 @@ app
     .post('/', (req, res) => {
         console.log("Post is called");
         const { name } = req.body;
-        const newGame = { name, id: ++counter }
+        const newGame = { name, id: ++counter };
         games.push(newGame)
         res.status(201).send(newGame)
     })
     .put('/:id', (req, res) => {
-        console.log(req.params)
         const { id } = req.params;
         console.log("Put is called");
+        const { name } = req.body;
+        const gameIndex = games.findIndex((game) => game.id === parseInt(id))
+        if (gameIndex != -1) {
+            games[gameIndex].name = name;
+            res.status(200).send(games[gameIndex])
+        } else {
+            const newGame = { name, id: ++counter };
+            games.push(newGame)
+            res.status(201).send(newGame)
+        }
+    })
+    .patch('/:id', (req, res) => {
+        console.log("Patch is called");
+        const { id } = req.params;
         const { name } = req.body;
         const gameIndex = games.findIndex((game) => game.id === parseInt(id))
         if (gameIndex != -1) {
@@ -39,13 +52,16 @@ app
             res.status(404).send({ "error": "Game with this id doesnt exist" })
         }
     })
-    .patch('/', (req, res) => {
-        console.log("Patch is called");
-        res.send("Patch called")
-    })
-    .delete('/', (req, res) => {
+    .delete('/:id', (req, res) => {
         console.log("Delete is called");
-        res.send("Delete called")
+        const { id } = req.params;
+        const gameIndex = games.findIndex((game) => game.id === parseInt(id))
+        if (gameIndex != -1) {
+            games = games.filter(game => game.id !== parseInt(id))
+            res.json({ "message": "Deleted Successfully" })
+        } else {
+            res.status(404).send({ "error": "Game with this id doesnt exist" })
+        }
     })
 
 app.listen(4000, () => console.log(`Server started at http://localhost:4000`))
